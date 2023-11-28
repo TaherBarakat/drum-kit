af877872d45874224bc7ddf49b3851b0cdefef0f
Taher Barakat <taher.barakat12223@gmail.com>
2023-11-28 18:19:57 +0300

    add eventlistner to play the sound whenever i presss a key

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-window.addEventListener('keydown',(e)=>console.log(e))
+window.addEventListener("keydown", (e) => {
+  let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.play();
+  audio.currentTime = 0;
+});
