b60480bdb6675d2f94a9ee1ba28b687c6a0ec807
Taher Barakat <taher.barakat12223@gmail.com>
2023-11-29 18:16:27 +0300

    add animation

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
-// window.addEventListener("keydown", (e) => {
-//   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//   if (!audio) return;
-//   audio.play();
-//   audio.currentTime = 0;
-// });
-
-// let click = function (e) {
-//   {
-//     console.log(e.target);
-//     let audio = document.querySelector(
-//       `audio[data-key="${e.target.getAttribute("data-key")}"]`
-//     );
-//     if (!audio) return;
-//     audio.play();
-//     audio.currentTime = 0;
-//   }
-// };
-
-// keys.forEach((key) => key.addEventListener("mousedown", click, true));
-
-// -----------------------------------
-
 window.addEventListener("keydown", (e) => {
-  console.log(e);
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-  console.log(audio);
-  console.log(audio.currentTime);
-  audio.currentTime = 0;
+  let pressedKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  pressedKey.classList.add("playing");
 
+  if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
-  console.log(audio.currentTime);
 });
 
 let keys = document.querySelectorAll(".key");
 
 keys.forEach((key) =>
   key.addEventListener("touchstart", (e) => {
-    console.log(e);
     let audio = document.querySelector(
       `audio[data-key="${e.target.getAttribute("data-key")}"]`
     );
+    let pressedKey = e.target;
+    pressedKey.classList.add("playing");
     if (!audio) return;
     audio.play();
     audio.currentTime = 0;
+  })
+);
+
+keys.forEach((key) =>
+  key.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
   })
 );
